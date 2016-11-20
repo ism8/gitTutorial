00a568c36eccaec8fd9b8c558df15662b9721767
ism8 <ism8005@thi.de>
2016-11-20 23:10:57 +0100

    hello sat

--- firstGitTest/src/firstGitTest/Hello.java
+++ firstGitTest/src/firstGitTest/Hello.java
@@ -3,7 +3,7 @@ package firstGitTest;
 public class Hello {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+           System.out.println("Helloo");
 
     }
 
